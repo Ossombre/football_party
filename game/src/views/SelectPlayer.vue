695fadbd8f335246365cd12e063d460649018ea8
driLegrand <adrien.demagny.com@gmail.com>
2020-04-13 00:48:00 +0200

    select players with pictures of players and some design

--- game/src/views/SelectPlayer.vue
+++ game/src/views/SelectPlayer.vue
@@ -1,49 +1,80 @@
 <template>
   <v-container>
     <v-row>
-      <v-col align-self="start">
-        <v-row v-for='data in players.data' :key="data.name">
-          <v-card v-on:click="selectPlayer" style="margin-bottom:20px">
-            <v-card-title>
-              {{data.name}}
-            </v-card-title>
-          </v-card>
-        </v-row>
+      <v-col>
+        <v-card color='orange' style="padding:20px">
+          <v-row justify="center">
+            <h1 style="font-color:blue">Player 1</h1>
+          </v-row>
+          <v-row dense>
+            <v-col align-self="start" cols="7">
+              <v-row v-for='data in players.data' :key="data.name">
+                <v-col>
+                  <v-card v-on:click="selectPlayer"  :dark="selectLeft.name == data.name" :disabled="selectRight.name == data.name">
+                    <v-card-title>
+                      {{data.name.replace('_',' ')}}
+                    </v-card-title>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-col>
+            <v-col cols="5">
+              <v-row>
+                <v-col>
+                  <v-card v-if="selectLeft != ''">
+                    <v-img
+                      :src='require("../assets/" + selectLeft.name + ".png")'>
+                    </v-img>
+                    <v-card-title v-html="selectLeft.name.replace('_',' ').replace(' ','<br>')">
+                    </v-card-title>
+                    <v-card-text>
+                      Speed: {{selectLeft.speed}} <br>
+                      Height: {{selectLeft.height}} <br>
+                    </v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-card>
       </v-col>
       <v-col>
-        <v-row>
-          <v-card v-if="selectLeft != ''">
-            <v-card-title>
-              {{selectLeft.name}}
-            </v-card-title>
-            <v-card-text>
-              Speed: {{selectLeft.speed}} <br>
-              Height: {{selectLeft.height}} <br>
-            </v-card-text>
-          </v-card>
-        </v-row>
-      </v-col>
-      <v-col>
-        <v-row align="baseline">
-          <v-card v-if="selectRight != ''" >
-            <v-card-title>
-              {{selectRight.name}}
-            </v-card-title>
-            <v-card-text>
-              Speed: {{selectRight.speed}} <br>
-              Height: {{selectRight.height}} <br>
-            </v-card-text>
-          </v-card>
-        </v-row>
-      </v-col>
-      <v-col align-self="end">
-        <v-row  v-for='player in players.data' :key="player.name">
-          <v-card v-on:click="selectPlayer" style="margin-bottom:20px">
-            <v-card-title>
-              {{player.name}}
-            </v-card-title>
-          </v-card>
-        </v-row>
+        <v-card color="orange"  style="padding:20px">
+          <v-row justify="center">
+            <h1 style="font-color:red">Player 2</h1>
+          </v-row>
+          <v-row>
+            <v-col cols="5">
+              <v-row align="baseline">
+                <v-col>
+                  <v-card v-if="selectRight != ''">
+                    <v-img
+                      :style="`transform: rotate(180deg);`"
+                      :src='require("../assets/" + selectRight.name + ".png")'>
+                    </v-img>
+                    <v-card-title v-html="selectRight.name.replace('_',' ').replace(' ','<br>')">
+                    </v-card-title>
+                    <v-card-text>
+                      Speed: {{selectRight.speed}} <br>
+                      Height: {{selectRight.height}} <br>
+                    </v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-col>
+            <v-col align-self="end" cols="7">
+              <v-row  v-for='player in players.data' :key="player.name">
+                <v-col>
+                  <v-card v-on:click="selectPlayer" :dark="selectRight.name == player.name" :disabled="selectLeft.name == player.name">
+                  <v-card-title>
+                    {{player.name.replace('_',' ')}}
+                  </v-card-title>
+                </v-card>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-card>
       </v-col>
     </v-row>
     <v-snackbar v-model="displayAlert" color="red">
@@ -82,10 +113,11 @@ export default {
     selectPlayer: function (event) {
       var keys = Object.keys(this.players.data)
       for (var i = 0; i < keys.length; i++) {
-        if (this.players.data[keys[i]].name.replace(/\s/g, '') === event.toElement.innerText.replace(/\s/g, '')) {
-          if (event.path[3]._prevClass.endsWith('start')) {
+        if (this.players.data[keys[i]].name.replace('_', ' ').replace(/\s/g, '') === event.toElement.innerText.replace('_', ' ').replace(/\s/g, '')) {
+          console.log('ok')
+          if (event.path[4]._prevClass.endsWith('start')) {
             this.selectLeft = this.players.data[keys[i]]
-          } else if (event.path[3]._prevClass.endsWith('end')) {
+          } else if (event.path[4]._prevClass.endsWith('end')) {
             this.selectRight = this.players.data[keys[i]]
           }
         }
