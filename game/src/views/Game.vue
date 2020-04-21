763c788ed675dac0a965560dbf95c4294dd88861
TheFryyy <47969114+TheFryyy@users.noreply.github.com>
2020-04-21 19:26:26 +0200

    graphic changes
    the way the score is displayed has been changed

--- game/src/views/Game.vue
+++ game/src/views/Game.vue
@@ -1,24 +1,54 @@
 <template>
-  <v-container fluid style="margin-left: 100px">
-    <v-row>
-      <v-col cols="1" style="margin-left: 100px;margin-right:50px">
-      <v-row>
-        <v-card color="blue" height="600" width="200">
-          <v-img
-            :src='require("../assets/" + this.player[1].name + ".png")'>
-          </v-img>
-          <v-card-title v-html="this.player[1].name.replace('_',' ').replace(' ','<br>')">
-          </v-card-title>
-          <v-card-text>
-              Speed: {{this.player[1].speed}} <br>
-              Height: {{this.player[1].height}} <br>
-          </v-card-text>
-        </v-card>
-      </v-row>
-    </v-col>
-    <v-col cols="7">
-      <v-card flat="true">
-        <v-overlay style="text-align: center;margin-right: 50px" v-if="!start || this.$game.victory != ''" absolute="true">
+  <v-container fluid>
+    <v-row style="margin-left:100pt">
+      <v-col >
+        <v-row>
+          <v-card color="blue" height="125pt" width="400pt">
+            <v-row>
+              <v-col cols="4">
+                <v-img
+                  :src='require("../assets/" + $route.query.player1 + ".png")'>
+                </v-img>
+              </v-col>
+              <v-col cols="4" style="margin-top: 25pt;">
+                <v-card-title v-html="$route.query.player1.replace('_',' ').replace(' ','<br>')">
+                </v-card-title>
+              </v-col>
+              <v-col cols="4" style="margin-top: 40pt;">
+                <h1>{{getScoreP1()}}</h1>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-row>
+      </v-col>
+      <v-col>
+        <h1 style="margin-top:50pt;margin-left:170pt">VS</h1>
+      </v-col>
+      <v-col style="margin-right:120px;">
+        <v-row>
+          <v-card color="red" height="125pt" width="400pt">
+            <v-row>
+              <v-col cols="4" style="margin-top: 40pt;">
+                <h1 style="margin-left:75pt">{{getScoreP2()}}</h1>
+              </v-col>
+              <v-col cols="4" style="margin-top: 20pt;">
+                <v-card-title v-html="$route.query.player2.replace('_',' ').replace(' ','<br>')">
+                </v-card-title>
+              </v-col>
+              <v-col cols="4">
+                <v-img
+                  :style="`transform: rotate(180deg);`"
+                  :src='require("../assets/" + $route.query.player2 + ".png")'>
+                </v-img>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-row>
+      </v-col>
+    </v-row>
+    <v-row >
+      <v-card style="margin-left: 320pt">
+        <v-overlay style="text-align: center" v-if="!start || this.$game.victory != ''" absolute=true>
           <h1 style="display: block" v-if="this.$game.victory != ''">{{getVictory()}}</h1>
           <v-btn  v-on:click="startGame" style="display: inline-block">{{button}}</v-btn>
         </v-overlay>
@@ -27,29 +57,9 @@
         <img id="PictureP1" :src='require("../assets/" + $route.query.player2 + ".png")' style="display: none">
         <img id="PictureP2" :src='require("../assets/" + $route.query.player1 + ".png")' style="display: none">
       </v-card>
-    </v-col>
-    <v-col cols="1">
-      <v-row>
-        <v-card color="red" height="600" width="200">
-          <v-img
-            :src='require("../assets/" + this.player[0].name + ".png")'>
-          </v-img>
-          <v-card-title v-html="this.player[0].name.replace('_',' ').replace(' ','<br>')">
-          </v-card-title>
-          <v-card-text>
-              Speed: {{this.player[0].speed}} <br>
-              Height: {{this.player[0].height}} <br>
-          </v-card-text>
-        </v-card>
-      </v-row>
-    </v-col>
-      <div style="width: 100%;margin-left:450px">
-        <h1 style="display: inline-block;color: blue"><pre>{{$route.query.player1.replace('_',' ')}}:  {{ getScoreP1() }}</pre></h1>
-        <h1 style="margin-left: 100px; display: inline-block; color:red"><pre>{{ getScoreP2() }}  :{{$route.query.player2.replace('_',' ')}}</pre></h1>
-      </div>
     </v-row>
-    <v-row justify="center" style="padding-right: 300px;padding-top:30px">
-      <v-btn v-on:click="returnBack" color='orange'>
+    <v-row justify="center">
+      <v-btn style="margin-top:25pt" v-on:click="returnBack" color='orange'>
         Return to character selection
       </v-btn>
     </v-row>
@@ -57,7 +67,6 @@
 </template>
 <script>
 import axios from 'axios'
-
 export default {
   name: 'Game',
   data: () => ({
@@ -84,7 +93,6 @@ export default {
     startGame () {
       window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
                               window.webkitRequestAnimationFrame || window.msRequestAnimationFrame
-
       for (var k = 0; k < this.player.length; k++) {
         if (this.player[k].name === this.$route.query.player2) {
           this.player1Stat = this.player[k]
@@ -92,12 +100,10 @@ export default {
           this.player2Stat = this.player[k]
         }
       }
-
       const canvas = this.$refs.gameScreen
       this.ctx = canvas.getContext('2d')
       this.start = true
       this.button = 'restart'
-
       this.$game.start(this.player1Stat, this.player2Stat)
       this.$game.victory = ''
       requestAnimationFrame(this.gameLoop)
